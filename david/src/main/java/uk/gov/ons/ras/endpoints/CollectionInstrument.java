6af25196f7820239e0c45848b7b85c70490f0a73
David Carboni <david@carboni.io>
2017-02-16 16:25:09 +0000

    Added a POST endpoint for creating a new CI.

--- david/src/main/java/uk/gov/ons/ras/endpoints/CollectionInstrument.java
+++ david/src/main/java/uk/gov/ons/ras/endpoints/CollectionInstrument.java
@@ -10,8 +10,14 @@ public class CollectionInstrument {
 
     @RequestMapping(value = "/collectioninstrument/{id}", method = RequestMethod.GET)
     public String get(@PathVariable("id") int id) {
-        System.out.println("Repeat after me: Spring is not simple. It's 'easy' as in easy to make a mess, but it's not simple. It's complicated, heavyweight and duller than the inside of a dodo's colon.");
+        System.out.println("Repeat after me: Spring is not simple. It's 'easy' as in easy to make a mess, " +
+                "but it's not simple. It's complicated, heavyweight and duller than the inside of a dodo's colon.");
         return "The CI you are looking for is: " + id;
+    }
+
+    @RequestMapping(value = "/collectioninstrument", method = RequestMethod.POST)
+    public String add() {
+        return "Pretending to create a collection instrument.";
     }
 
 }
