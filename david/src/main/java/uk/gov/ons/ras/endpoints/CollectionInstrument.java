15a02abd68810c9a660733774408b0b0274f54d1
David Carboni <david@carboni.io>
2017-02-16 16:14:08 +0000

    First stab at the most useful endpoint.

---
+++ david/src/main/java/uk/gov/ons/ras/endpoints/CollectionInstrument.java
@@ -0,0 +1,17 @@
+package uk.gov.ons.ras.endpoints;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class CollectionInstrument {
+
+    @RequestMapping(value = "/collectioninstrument/{id}", method = RequestMethod.GET)
+    public String get(@PathVariable("id") int id) {
+        System.out.println("Repeat after me: Spring is not simple. It's 'easy' as in easy to make a mess, but it's not simple. It's complicated, heavyweight and duller than the inside of a dodo's colon.");
+        return "The CI you are looking for is: " + id;
+    }
+
+}
