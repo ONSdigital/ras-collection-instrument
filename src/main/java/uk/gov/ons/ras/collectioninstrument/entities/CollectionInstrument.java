67e6ba100778a66166475bfbe63acf27e51f3878
David Carboni <david@carboni.io>
2017-02-20 15:14:57 +0000

    Reformat/organise imports.

--- src/main/java/uk/gov/ons/ras/collectioninstrument/entities/CollectionInstrument.java
+++ src/main/java/uk/gov/ons/ras/collectioninstrument/entities/CollectionInstrument.java
@@ -1,53 +1,48 @@
 package uk.gov.ons.ras.collectioninstrument.entities;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import javax.persistence.*;
-import java.io.IOException;
 
 @Entity
 @Table(schema = "ras_collection_instrument",
-	   name = "ras_collection_instruments")
+        name = "ras_collection_instruments")
 public class CollectionInstrument {
 
-	public static class Json {
+    public static class Json {
         public String urn;
         public String ciType;
-		public String surveyId;
-		public String classifiers;
-	}
+        public String surveyId;
+        public String classifiers;
+    }
 
-	/**
-	 * The primary key for this CollectionInstrument.
-	 */
+    /**
+     * The primary key for this CollectionInstrument.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "G1")
     @SequenceGenerator(name = "G1", sequenceName = "ras_collection_instrument.ras_collection_instruments_id_seq")
     @Column(name = "ID", nullable = false, unique = true)
     private Long id;
-	
+
     /**
      * A string representation of the JSON content of this CollectionInstrument.
      */
-	@Column(name = "CONTENT", nullable = false, unique = false)
+    @Column(name = "CONTENT", nullable = false, unique = false)
     private String content;
-	private Json json;
-	
-    public Long getId() {
-		return id;
-	}
+    private Json json;
 
-	public String getContent() {
-		return content;
-	}
-	
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
 	/* Once content is assigned we can pull out the individual fields from the content e.g
-	{id: 2,
+    {id: 2,
 	 content: {
 		"id": "urn:ons.gov.uk:id:ci:001.001.00002",
 		"ciType": "OFFLINE",
@@ -57,59 +52,64 @@ public class CollectionInstrument {
 	}
 	
 	*/
-	
-	/**
-	 * urn of the CollectionInstrument.
-	 * @return
-	 */
-	public String getUrn() {
-		return parse().urn;
-	}
 
-	/**
-	 * Type of CollectionInstrument.
-	 * @return
-	 */
-	public String getCiType() {
-		return parse().ciType;
+    /**
+     * urn of the CollectionInstrument.
+     *
+     * @return
+     */
+    public String getUrn() {
+        return parse().urn;
+    }
 
-	}
+    /**
+     * Type of CollectionInstrument.
+     *
+     * @return
+     */
+    public String getCiType() {
+        return parse().ciType;
 
-	/**
-	 * Survey ID of this CollectionInstrument.
-	 * @return
-	 */
-	public String getSurveyId() {
-		return parse().surveyId;
-	}
+    }
 
-	/**
-	 * The array of Classifiers and their values for this CollectionInstrument.
-	 * @return
-	 */
-	public String getClassifiers() {
-		return parse().classifiers;
-	}
-	
-	/**
-	 * Parses the content to a Json structure.
-	 */
-	private Json parse() {
-		if (json == null) {
-			json = new Gson().fromJson(content, Json.class);
-		}
-		return json;
-	}
-	
+    /**
+     * Survey ID of this CollectionInstrument.
+     *
+     * @return
+     */
+    public String getSurveyId() {
+        return parse().surveyId;
+    }
+
+    /**
+     * The array of Classifiers and their values for this CollectionInstrument.
+     *
+     * @return
+     */
+    public String getClassifiers() {
+        return parse().classifiers;
+    }
+
+    /**
+     * Parses the content to a Json structure.
+     */
+    private Json parse() {
+        if (json == null) {
+            json = new Gson().fromJson(content, Json.class);
+        }
+        return json;
+    }
+
     /**
      * Overridden toString to convert to a JSON formatted string.
+     *
      * @return this object as a string
      */
     @Override
     public final String toString() {
-    	// Enable pretty printing
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		return gson.toJson(content);
+        // Enable pretty printing
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(content);
     }
 
 }
