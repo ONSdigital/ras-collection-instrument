d97e678e98af83be94db41cf7ab63c026eb74922
David Carboni <david@carboni.io>
2017-02-21 12:17:26 +0000

    Evicted Spring Cloud to speed up startup and clear a ton of exceptions in the log.

--- src/main/java/uk/gov/ons/ras/collectioninstrument/Application.java
+++ src/main/java/uk/gov/ons/ras/collectioninstrument/Application.java
@@ -3,11 +3,11 @@ package uk.gov.ons.ras.collectioninstrument;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
