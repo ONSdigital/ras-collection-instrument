3085b5ee708d4669195ac3fa4af8595f1c7b841a
David Carboni <david@carboni.io>
2017-02-21 12:18:15 +0000

    Simplified the bulid script to speed up tunraround time.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -it --rm --volume `pwd`:/root --volume $HOME/.gradle:/root/.gradle frekele/gradle gradle clean build
+gradle clean build
 docker build --tag ras-collection-instrument .
