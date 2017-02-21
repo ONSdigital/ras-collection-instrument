ac9685c489ab0e79acbde52985a3d3d5aac4cbdd
David Carboni <david@carboni.io>
2017-02-21 12:54:44 +0000

    Added port mappings to match ras-compose project.

--- run.sh
+++ run.sh
@@ -7,5 +7,4 @@ docker rm -f ras-collection-instrument
 network=`docker network ls --filter name=ras -q`
 
 # Run a container and capture the ID of the container:
-docker run -d --net $network --name ras-collection-instrument ras-collection-instrument
-
+docker run -d --net $network -p "8070:8080" -p "8001:8000" --name ras-collection-instrument ras-collection-instrument
