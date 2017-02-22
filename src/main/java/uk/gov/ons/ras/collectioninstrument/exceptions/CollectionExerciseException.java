d907e0e36af27cb472f3f7a7fa671e8c1e9a6cf1
David Carboni <david@carboni.io>
2017-02-22 09:43:16 +0000

    Commenting out for now.

--- src/main/java/uk/gov/ons/ras/collectioninstrument/exceptions/CollectionExerciseException.java
+++ src/main/java/uk/gov/ons/ras/collectioninstrument/exceptions/CollectionExerciseException.java
@@ -1,14 +1,14 @@
-package uk.gov.ons.ras.collectioninstrument.exceptions;
-
-public class CollectionExerciseException extends Exception {
-
-	/**
-	 * SerialVersionUID.
-	 */
-	private static final long serialVersionUID = 1L;
-
-    public CollectionExerciseException(String message) {
-        super(message);
-    }
-	
-}
+//package uk.gov.ons.ras.collectioninstrument.exceptions;
+//
+//public class CollectionExerciseException extends Exception {
+//
+//	/**
+//	 * SerialVersionUID.
+//	 */
+//	private static final long serialVersionUID = 1L;
+//
+//    public CollectionExerciseException(String message) {
+//        super(message);
+//    }
+//
+//}
