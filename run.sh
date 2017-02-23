ec2a6c716e408625c0080e8b5305faff06c2079c
David Carboni <david@carboni.io>
2017-02-23 13:10:48 +0000

    Updated container run command.

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ docker rm -f $name
 network=`docker network ls --filter name=ras -q`
 
 # Run a container and capture the ID of the container:
-docker run -d --net $network -p "8070:8080" -p "8001:8000" -e spring.datasource.url=jdbc:postgresql://postgres:5432/postgres --name $name $name
+docker run -d --net $network -p "8070:5000" --name $name $name
 Echo Tailing Docker log for $name. Ctrl-C will stop tailing but will leave the container running.
 docker logs -f $name
