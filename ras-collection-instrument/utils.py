0aa976f19ab9e6c74bb02210995db4cffe0bbb44
Charles <enquiries@movingdigi.com>
2017-03-13 14:27:54 +0000

    pep8

--- ras-collection-instrument/utils.py
+++ ras-collection-instrument/utils.py
@@ -1,16 +1,13 @@
-from flask import request
-
-authheader = request.headers.get('authorization')
+"""
+Util module
+"""
 
 from jwt import decode
 
+
 def get_jwt_from_header(auth_header_value):
-	jwt_dict = decode(auth_header)
-	return jwt_dict
-
-
-"""
-data_dict_for_jwt_token = {"user_id": "c3c0c2cd-bd52-428f-8841-540b1b7dd619",
-                           "user_scopes": ['foo', 'bar', 'qux']
-                           }
-"""
+    """
+    returns jwt token out of the auth header
+    """
+    jwt_dict = decode(auth_header_value)
+    return jwt_dict
