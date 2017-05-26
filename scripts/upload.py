023a10e8909a1b062c7ad7a7f86d28fb5b9e9ba9
Gareth Bult <gareth@bult.co.uk>
2017-05-26 14:12:40 +0100

    Upping memory on CF manifest

--- scripts/upload.py
+++ scripts/upload.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from os import getenv
-from sys import argv
+from sys import argv, stdout
 import requests
 
 def usage():
@@ -12,14 +12,16 @@ if len(argv) < 4:
     usage()
 
 host = argv[1]
-if host not in ['local', 'live']:
+if host not in ['local', 'live', 'dev']:
     print('%% expecting either "local" or "live" for host')
     exit(1)
 
 if host == 'local':
     host = 'http://localhost:8080'
-else:
+elif host == 'live':
     host = 'http://ras-collection-instrument-demo.apps.mvp.onsclofo.uk'
+else: 
+    host = 'http://ras-collection-instrument-dev.apps.mvp.onsclofo.uk'
 
 ref = argv[2]
 count = int(argv[3])
@@ -28,10 +30,13 @@ API_UPLOAD = getenv('API_UPLOAD', '{}/collection-instrument-api/1.0.2/upload/{}/
 
 actual = 0
 for i in range(0, count):
+    #files = {'files[]': ('upload.txt', open('django_demo.proj', 'rb'), 'text/html', {'Expires': 0})}
     files = {'files[]': ('upload.txt', open('upload.txt', 'rb'), 'text/html', {'Expires': 0})}
     r = requests.post(API_UPLOAD, files=files)
     if r.status_code != 200:
         print('%% upload error "{}" - "{}"'.format(r.status_code, r.text))
         exit(1)
+    print(".", end='')
+    stdout.flush()
 
 print(r.text)
