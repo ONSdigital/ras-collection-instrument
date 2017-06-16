dc57903cc5c0424218c72344a7071e15a9643fa1
Gareth Bult <gareth@bult.co.uk>
2017-06-16 12:04:28 +0100

    Conflict resolution on __main__.py

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,28 +1,17 @@
-#!/usr/bin/env python3
 ##############################################################################
 #                                                                            #
 #   Microservices header template                                            #
-#   Date:    18 May 2017                                                     #
-#   Author:  Gareth Bult                                                     #
 #   License: MIT                                                             #
 #   Copyright (c) 2017 Crown Copyright (Office for National Statistics)      #
 #                                                                            #
 ##############################################################################
-from connexion import App
-from flask_cors import CORS
-from .encoder import JSONEncoder
-from .configuration import ons_env
+from ons_ras_common import ons_env
 
 if __name__ == '__main__':
-    ons_env.activate()
-    app = App(__name__, specification_dir='./swagger/')
-    CORS(app.app)
-    app.app.json_encoder = JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'ONS Microservice'})
-    app.run(host='0.0.0.0', port=ons_env.port)
 
-    #from twisted.internet import reactor
-    #from flask_twisted import Twisted
-    #reactor.callLater(1, print, '<<Twisted is running>>')
-    #Twisted(app).run(port=getenv('PORT',8080))
-    # Comment
+    def callback(app):
+        from swagger_server.controllers.exceptions import SessionScopeException
+        from swagger_server.controllers.error_handlers import session_scope_handler
+        app.app.register_error_handler(SessionScopeException, session_scope_handler)
+
+    ons_env.activate(callback)
