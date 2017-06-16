8b10d24a2beee4ad8285a8481489cd45fe1975e3
Gareth Bult <gareth@bult.co.uk>
2017-06-16 11:56:14 +0100

    Fixing unit tests

--- swagger_server/test/__init__.py
+++ swagger_server/test/__init__.py
@@ -9,7 +9,11 @@ from swagger_server.controllers.error_handlers import session_scope_handler
 class BaseTestCase(TestCase):
 
     def create_app(self):
-        ons_env.setup()
+        ons_env.setup_ini()
+        ons_env.db.activate()
+        ons_env._jwt.activate()
+        ons_env._cryptography.activate()
+
         logging.getLogger('connexion.operation').setLevel('ERROR')
         app = connexion.App(__name__, specification_dir='../swagger/')
         app.add_api('swagger.yaml')
