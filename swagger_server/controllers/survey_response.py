a8155c342c7214e22fb64e01891ff34fa8e24fb1
Rhiannon Griffiths <rhiannon.griffiths@ons.gov.uk>
2017-09-13 12:06:26 +0100

    modifying constant file error strings to match designs for error displays

--- swagger_server/controllers/survey_response.py
+++ swagger_server/controllers/survey_response.py
@@ -11,8 +11,8 @@ from swagger_server.controllers.submitter.rabbitmq_submitter import RabbitMQSubm
 from swagger_server.controllers.exceptions import UploadException
 from werkzeug.utils import secure_filename
 
-FILE_EXTENSION_ERROR = 'un-accepted file extension'
-FILE_NAME_LENGTH_ERROR = 'The name of the file is too long'
+FILE_EXTENSION_ERROR = 'The spreadsheet must be in .xls ot .xlsx format'
+FILE_NAME_LENGTH_ERROR = 'The file name of your spreadsheet must be less than 50 characters long'
 UPLOAD_SUCCESSFUL = 'Upload successful'
 UPLOAD_UNSUCCESSFUL = 'Upload failed'
 INVALID_UPLOAD = 'The upload must have valid case_id and a file attached'
