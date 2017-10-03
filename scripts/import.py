d55bdb3122698e99d9c57523b985fd691e600fc9
Andrew Millar <andrew.millar@qa.com>
2017-10-03 14:00:22 +0100

    removing default collection instrument host and collection exercise id, now getting them from environment

--- scripts/import.py
+++ scripts/import.py
@@ -9,28 +9,18 @@ import requests
 import xlsxwriter
 
 
-COLLECTION_EXERCISE = '14fb3e68-4dca-46db-bf49-04b84e07e77c'
-# COLLECTION_EXERCISE = 'c6467711-21eb-4e78-804c-1db8392f93fb'
-# COLLECTION_EXERCISE = "c6467711-21eb-4e78-804c-1db8392f93bb"
-# COLLECTION_EXERCISE = "c6467711-21eb-4e78-804c-1db8392f93aa"
 API_UPLOAD = '{}/collection-instrument-api/1.0.2/upload/{}/{}'
 TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-urls = {
-    'dev': 'http://api-dev.apps.devtest.onsclofo.uk',
-    'local': 'http://localhost:8002',
-    'test': 'http://ras-collection-instrument-test.apps.devtest.onsclofo.uk',
-    'int': 'http://ras-collection-instrument-int.apps.devtest.onsclofo.uk',
-    'demo': 'http://ras-collection-instrument-demo.apps.devtest.onsclofo.uk',
-    'sit': 'http://ras-collection-instrument-sit.apps.devtest.onsclofo.uk',
-    'ci': 'http://ras-collection-instrument-ci.apps.devtest.onsclofo.uk'
-}
+COLLECTION_EXERCISE_ID = os.getenv('COLLECTION_EXERCISE_ID')
+HOST = os.getenv('COLLECTION_INSTRUMENT_HOST')
 
-if len(sys.argv) < 2 or sys.argv[1] not in urls:
-    print("Please specify an environment from;")
-    print(urls.keys())
+if not HOST:
+    print("Please specify the collection instrument host with environment variable; 'COLLECTION_INSTRUMENT_HOST'")
+    sys.exit(1)
+if not COLLECTION_EXERCISE_ID:
+    print("Please specify the collection exercise id with environment variable; 'COLLECTION_EXERCISE_ID'")
     sys.exit(1)
 
-HOST = urls[sys.argv[1]]
 
 with open('ru_ref_import.txt') as io:
     while True:
@@ -49,7 +39,7 @@ with open('ru_ref_import.txt') as io:
         worksheet.write('A1', 'RU_REF = {}'.format(ru_ref))
         workbook.close()
 
-        url = API_UPLOAD.format(HOST, COLLECTION_EXERCISE, fname)
+        url = API_UPLOAD.format(HOST, COLLECTION_EXERCISE_ID, fname)
         basic_auth = (os.getenv('SECURITY_USER_NAME'), os.getenv('SECURITY_USER_PASSWORD'))
         files = {'files[]': (fname, open(fname, 'rb'), TYPE, {'Expires': 0})}
         r = requests.post(url, auth=basic_auth, files=files, verify=False)
