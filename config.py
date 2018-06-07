3dfd547b1f3c2bef44afd87283a269d71abd19f1
Tejas Patel <tejas.patel@ons.gov.uk>
2018-06-07 15:01:06 +0100

    remove unnecessary test config

--- config.py
+++ config.py
@@ -82,6 +82,3 @@ class TestingConfig(Config):
     DATABASE_SCHEMA = 'ras_ci'
     ONS_CRYPTOKEY = 'somethingsecure'
     JSON_SECRET_KEYS = open("./tests/files/keys.json").read()
-    DB_POOL_SIZE = 5
-    DB_MAX_OVERFLOW = 10
-    DB_POOL_RECYCLE = -1
