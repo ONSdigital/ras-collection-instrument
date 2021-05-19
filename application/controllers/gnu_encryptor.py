b288e673cb695664b9895125db8bfebf8f634bf6
David Morgan <davidmorgan@ext.ons.gov.uk>
2021-05-19 14:26:04 +0100

    pep8

--- application/controllers/gnu_encryptor.py
+++ application/controllers/gnu_encryptor.py
@@ -12,13 +12,14 @@ class GNUEncrypter:
         Encrypts the payload using the recipient values
 
         :param payload: the value to encrypt
-        :param recipient: who is it for 
+        :param recipient: who is it for
         :return: string of encrypted data
         """
 
         enc_data = self.gpg.encrypt(payload, recipient, always_trust=True)
         if not enc_data.ok:
-            raise ValueError('Failed to GNU encrypt bag: {}.  Have you installed a required public key?'.format(enc_data.status))
+            raise ValueError('Failed to GNU encrypt bag: {}.'\
+                             '  Have you installed a required public key?'.format(enc_data.status))
         return str(enc_data)
 
     def decrypt(self, payload, passphrase):
@@ -26,13 +27,13 @@ class GNUEncrypter:
         Decrypt the payload using the passphase values
 
         :param payload: the value to decrypt
-        :param recipient: who is it for 
+        :param recipient: who is it for
         :return: string of decrypted data
         """
 
         decrypted_data = self.gpg.decrypt(payload, passphrase=None)
         if not decrypted_data.ok:
-            raise ValueError('Failed to GNU encrypt bag: {}.  Have you installed a required public key?'.format(decrypted_data.status))
+            raise ValueError('Failed to GNU encrypt bag: {}.'\
+                             '  Have you installed a required public key?'.format(decrypted_data.status))
         return str(decrypted_data)
-
 
