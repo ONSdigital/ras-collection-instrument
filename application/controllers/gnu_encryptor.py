56f335cdfdd755849e1764f3889b6266a3807dcf
David Morgan <davidmorgan@ext.ons.gov.uk>
2021-05-27 12:56:06 +0100

    set a byte-like obj

--- application/controllers/gnu_encryptor.py
+++ application/controllers/gnu_encryptor.py
@@ -5,8 +5,7 @@ class GNUEncrypter:
 
     def __init__(self, public_key, passphrase=None, always_trust=True):
         self.gpg = gnupg.GPG()
-        self.gpg.import_keys(public_key)
-
+        self.gpg.import_keys(public_key.encode('utf-8'))
     def encrypt(self, payload, recipient):
         """
         Encrypts the payload using the recipient values
