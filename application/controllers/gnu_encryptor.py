8149944a08f50442e58cdaa0000894d341f9f0ab
David Morgan <davidmorgan@ext.ons.gov.uk>
2021-05-27 16:26:07 +0100

    dict to bytes

--- application/controllers/gnu_encryptor.py
+++ application/controllers/gnu_encryptor.py
@@ -18,7 +18,7 @@ class GNUEncrypter:
         print(type(payload))
         print(payload)
         print(type(recipient))
-        enc_data = self.gpg.encrypt(payload.encode('utf-8'), recipient, always_trust=True)
+        enc_data = self.gpg.encrypt(json.dumps(payload).encode('utf-8'), recipient, always_trust=True)
         if not enc_data.ok:
             raise ValueError('Failed to GNU encrypt bag: {}.'
                              '  Have you installed a required public key?'.format(enc_data.status))
