d1a752c852d6ecd6bd3e7d4d98dcb54d96ae6663
David Morgan <davidmorgan@ext.ons.gov.uk>
2021-05-28 08:30:46 +0100

    flak8 whitespacw

--- application/controllers/gnu_encryptor.py
+++ application/controllers/gnu_encryptor.py
@@ -18,13 +18,13 @@ class GNUEncrypter:
         """
         enc_data = self.gpg.encrypt(json.dumps(payload).encode('utf-8'), recipient, always_trust=True)
         if not enc_data.ok:
-            print ('ok: ', enc_data.ok)
-            print ('status: ', enc_data.status)
-            print ('stderr: ', enc_data.stderr)
-            print ('unencrypted_string: ', payload)
-            print ('encrypted_string: ', enc_data)
-            print ('payload:',payload)
-            print ('recipient:',recipient)
+            print('ok: ', enc_data.ok)
+            print('status: ', enc_data.status)
+            print('stderr: ', enc_data.stderr)
+            print('unencrypted_string: ', payload)
+            print('encrypted_string: ', enc_data)
+            print('payload:', payload)
+            print('recipient:', recipient)
             raise ValueError('Failed to GNU encrypt bag: {}.'
                              '  Have you installed a required public key?'.format(enc_data.status))
         return str(enc_data)
