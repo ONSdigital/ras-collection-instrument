7e8f29fe7ffef095b03f1f696bbbe431e0f33a82
Adam Wilkie <Adam.Wilkie@ons.gov.uk>
2021-10-01 11:17:25 +0100

    rewrite code to check for GCS value when downloading

--- application/controllers/collection_instrument.py
+++ application/controllers/collection_instrument.py
@@ -138,6 +138,7 @@ class CollectionInstrument(object):
 
         seft_file = self._create_seft_file(instrument.instrument_id, file)
         instrument.seft_file = seft_file
+        instrument.seft_file.gcs = True
 
         exercise = self._find_or_create_exercise(exercise_id, session)
         instrument.exercises.append(exercise)
@@ -459,48 +460,25 @@ class CollectionInstrument(object):
             return None
 
         for instrument in exercise.instruments:
-            csv += csv_format.format(
-                count=count,
-                file_name=instrument.name,
-                length=instrument.seft_file.len if instrument.seft_file else None,
-                date_stamp=instrument.stamp,
-            )
-            count += 1
-        return csv
-
-    @staticmethod
-    @with_db_session
-    def get_instruments_by_exercise_id_csv_from_bucket(exercise_id, session=None):
-        """
-        Finds all collection instruments associated with an exercise and returns them in csv format
-
-        :param exercise_id
-        :param session: database session
-        :return: collection instruments in csv
-        """
-        log.info("Getting csv for instruments", exercise_id=exercise_id)
-
-        validate_uuid(exercise_id)
-        csv_format = '"{count}","{file_name}","{length}","{date_stamp}"\n'
-        count = 1
-        csv = csv_format.format(count="Count", file_name="File Name", length="Length", date_stamp="Time Stamp")
-        exercise = query_exercise_by_id(exercise_id, session)
-
-        if not exercise:
-            return None
-
-        for instrument in exercise.instruments:
-            try:
-                seft_ci_bucket = GoogleCloudSEFTCIBucket(current_app.config)
-                file = seft_ci_bucket.download_file_from_bucket(instrument.file_location)
+            if instrument.seft_file.gcs:
+                try:
+                    seft_ci_bucket = GoogleCloudSEFTCIBucket(current_app.config)
+                    file = seft_ci_bucket.download_file_from_bucket(instrument.seft_file.file_name)
+                    csv += csv_format.format(
+                        count=count,
+                        file_name=instrument.seft_file.file_name,
+                        length=len(file),
+                        date_stamp=instrument.stamp,
+                    )
+                except Exception:
+                    log.exception("Couldn't find SEFT CI from bucket")
+            else:
                 csv += csv_format.format(
                     count=count,
-                    file_name=instrument.file_location,
-                    length=len(file),
+                    file_name=instrument.name,
+                    length=instrument.seft_file.len if instrument.seft_file else None,
                     date_stamp=instrument.stamp,
                 )
-            except Exception:
-                log.exception("Couldn't find SEFT CI from bucket")
             count += 1
         return csv
 
@@ -523,7 +501,7 @@ class CollectionInstrument(object):
     @with_db_session
     def get_instrument_data(instrument_id, session):
         """
-        Get the instrument data from the db using the id
+        Get the instrument data from the db or bucket using the id
 
         :param instrument_id: The id of the instrument we want
         :param session: database session
@@ -536,35 +514,18 @@ class CollectionInstrument(object):
         file_name = None
 
         if instrument:
-            log.info("Decrypting collection instrument data", instrument_id=instrument_id)
-            cryptographer = Cryptographer()
-            data = cryptographer.decrypt(instrument.seft_file.data)
-            file_name = instrument.seft_file.file_name
-        return data, file_name
-
-    @staticmethod
-    @with_db_session
-    def get_instrument_data_from_database(instrument_id, session):
-        """
-        Get the instrument data from the db using the id
-
-        :param instrument_id: The id of the instrument we want
-        :param session: database session
-        :return: data and file_name
-        """
-
-        instrument = CollectionInstrument.get_instrument_by_id(instrument_id, session)
-
-        data = None
-        file_name = None
-
-        if instrument.file_location:
-            try:
-                seft_ci_bucket = GoogleCloudSEFTCIBucket(current_app.config)
-                file = seft_ci_bucket.download_file_from_bucket(instrument.file_location)
-                return file, instrument.file_location
-            except Exception:
-                log.exception("Couldn't find SEFT CI in GCP bucket")
+            if instrument.seft_file.gcs:
+                try:
+                    seft_ci_bucket = GoogleCloudSEFTCIBucket(current_app.config)
+                    file = seft_ci_bucket.download_file_from_bucket(instrument.seft_file.file_name)
+                    return file, instrument.seft_file.file_name
+                except Exception:
+                    log.exception("Couldn't find SEFT CI in GCP bucket")
+            else:
+                log.info("Decrypting collection instrument data", instrument_id=instrument_id)
+                cryptographer = Cryptographer()
+                data = cryptographer.decrypt(instrument.seft_file.data)
+                file_name = instrument.seft_file.file_name
         return data, file_name
 
     @staticmethod
