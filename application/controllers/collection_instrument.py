4df4848fd42f451d2a65a2c3f74dd1d6c09e37b0
anwilkie <47449873+anwilkie@users.noreply.github.com>
2021-10-15 10:58:43 +0100

    Seft ci subfolder stuff (#255)
    * initial commit
    
    * adjustment to bucket file path
    
    * auto patch increment
    
    * go back to only passing in one parameter
    
    * update patch endpoint to support instruments in GCS
    
    * auto patch increment
    
    * correctly store bucket path to file in seft instrument table
    
    * attempt at adding code to handle reconstructing file path when downloading
    
    * attempt 2 at reconstructing path
    
    * attempt 3 at reconstructing path
    
    * cleanup
    
    * auto patch increment
    
    * created initial attempt at adding logic for reconstructing file path
    
    * Fix a test
    
    * fixed errors in tests
    
    * auto patch increment
    
    * fix mistake with file path prefixes
    
    * adding code to delete old file from bucket when updating file
    
    * log exception
    
    * do delete before upload when updating file
    
    * fix exception error
    
    Co-authored-by: ras-rm-pr-bot <matt.crocker+rasrm@gmail.com>
    Co-authored-by: Amit Sinha <amit@creativeappdevs.com>
    Co-authored-by: Adam Mann <manny160389@hotmail.com>

--- application/controllers/collection_instrument.py
+++ application/controllers/collection_instrument.py
@@ -8,6 +8,7 @@ from application.controllers.cryptographer import Cryptographer
 from application.controllers.helper import validate_uuid
 from application.controllers.service_helper import (
     collection_instrument_link,
+    get_survey_ref,
     service_request,
 )
 from application.controllers.session_decorator import with_db_session
@@ -132,9 +133,6 @@ class CollectionInstrument(object):
 
         seft_file = self._create_seft_file(instrument.instrument_id, file, encrypt_and_save_to_db=False)
         instrument.seft_file = seft_file
-        seft_ci_bucket = GoogleCloudSEFTCIBucket(current_app.config)
-        seft_ci_bucket.upload_file_to_bucket(file=file)
-        instrument.seft_file.gcs = True
 
         exercise = self._find_or_create_exercise(exercise_id, session)
         instrument.exercises.append(exercise)
@@ -149,6 +147,16 @@ class CollectionInstrument(object):
 
         if classifiers:
             instrument.classifiers = loads(classifiers)
+
+        try:
+            survey_ref = get_survey_ref(instrument.survey.survey_id)
+            file.filename = survey_ref + "/" + exercise_id + "/" + file.filename
+            seft_ci_bucket = GoogleCloudSEFTCIBucket(current_app.config)
+            seft_ci_bucket.upload_file_to_bucket(file=file)
+            instrument.seft_file.gcs = True
+        except Exception as e:
+            log.exception("An error occurred when trying to put SEFT CI in bucket")
+            raise e
 
         session.add(instrument)
         return instrument
@@ -172,7 +180,10 @@ class CollectionInstrument(object):
             log.error("Not a SEFT instrument")
             raise RasError("Not a SEFT instrument", 400)
 
-        seft_model = self._update_seft_file(instrument.seft_file, file)
+        survey_ref = get_survey_ref(instrument.survey.survey_id)
+        exercise_id = str(instrument.exids[0])
+
+        seft_model = self._update_seft_file(instrument.seft_file, file, survey_ref, exercise_id)
         session.add(seft_model)
 
     @staticmethod
@@ -416,7 +427,7 @@ class CollectionInstrument(object):
         return seft_file
 
     @staticmethod
-    def _update_seft_file(seft_model, file):
+    def _update_seft_file(seft_model, file, survey_ref, exercise_id):
         """
         Updates a seft_file with a new version of the data
 
@@ -435,9 +446,12 @@ class CollectionInstrument(object):
             seft_model.file_name = file.filename
             seft_model.data = encrypted_file
         else:
-            # ToDo : Add logic for path for file
+            old_filename = seft_model.file_name
             seft_model.file_name = file.filename
+            file.filename = survey_ref + "/" + exercise_id + "/" + file.filename
+            old_filename = survey_ref + "/" + exercise_id + "/" + old_filename
             seft_ci_bucket = GoogleCloudSEFTCIBucket(current_app.config)
+            seft_ci_bucket.delete_file_from_bucket(old_filename)
             seft_ci_bucket.upload_file_to_bucket(file=file)
             seft_model.gcs = True
         return seft_model
@@ -466,8 +480,11 @@ class CollectionInstrument(object):
         for instrument in exercise.instruments:
             if instrument.seft_file.gcs:
                 try:
+                    survey_ref = get_survey_ref(instrument.survey.survey_id)
+                    exercise_id = str(instrument.exids[0])
+                    file_path = survey_ref + "/" + exercise_id + "/" + instrument.seft_file.file_name
                     seft_ci_bucket = GoogleCloudSEFTCIBucket(current_app.config)
-                    file = seft_ci_bucket.download_file_from_bucket(instrument.seft_file.file_name)
+                    file = seft_ci_bucket.download_file_from_bucket(file_path)
                     csv += csv_format.format(
                         count=count,
                         file_name=instrument.seft_file.file_name,
@@ -520,8 +537,11 @@ class CollectionInstrument(object):
         if instrument:
             if instrument.seft_file.gcs:
                 try:
+                    survey_ref = get_survey_ref(instrument.survey.survey_id)
+                    exercise_id = str(instrument.exids[0])
+                    file_path = survey_ref + "/" + exercise_id + "/" + instrument.seft_file.file_name
                     seft_ci_bucket = GoogleCloudSEFTCIBucket(current_app.config)
-                    file = seft_ci_bucket.download_file_from_bucket(instrument.seft_file.file_name)
+                    file = seft_ci_bucket.download_file_from_bucket(file_path)
                     return file, instrument.seft_file.file_name
                 except Exception:
                     log.exception("Couldn't find SEFT CI in GCP bucket")
