aa0381cb9b3aeb1f815121e7bb4b2bf76a488ca7
Adam Wilkie <Adam.Wilkie@ons.gov.uk>
2021-10-21 10:15:15 +0100

    initial commit

---
+++ migrations/versions/e2012ed329da_remove_unused_columns.py
@@ -0,0 +1,30 @@
+"""add seft ci file location column
+
+Revision ID: e2012ed329da
+Revises: d975ea83b712
+Create Date: 2021-10-21 10:01:56.565921
+
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.types import Enum, Integer
+
+# revision identifiers, used by Alembic.
+revision = "e2012ed329da"
+down_revision = "d975ea83b712"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_column("exercise", "items")
+    op.drop_column("exercise", "status")
+
+
+def downgrade():
+    op.add_column("exercise", sa.Column("items", Integer), schema="ras_ci")
+    op.add_column(
+        "exercise",
+        sa.Column("status", Enum("uploading", "pending", "active", name="status")),
+        schema="ras_ci",
+    )
