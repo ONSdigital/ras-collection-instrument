589a13def4af5791b89a8941053ea4409203dd1b
Adam Wilkie <Adam.Wilkie@ons.gov.uk>
2021-10-26 16:41:57 +0100

    revert minor change to unit test

--- tests/views/test_collection_instrument_view.py
+++ tests/views/test_collection_instrument_view.py
@@ -209,7 +209,7 @@ class TestCollectionInstrumentView(TestClient):
         with patch("application.controllers.collection_instrument.service_request", return_value=mock_survey_service):
             # When a post is made to the upload end point
             response = self.client.post(
-                "/collection-instrument-api/1.0.2/upload/cb0711c3-0ac8-41d3-ae0e-567e5ea1ef87/12345678900",
+                "/collection-instrument-api/1.0.2/upload/cb0711c3-0ac8-41d3-ae0e-567e5ea1ef87/12345678901",
                 headers=self.get_auth_headers(),
                 data=data2,
                 content_type="multipart/form-data",
