1056dc02ccb4dd07e93c8cafa7d77552b0853996
David Morgan <davidmorgan@ext.ons.gov.uk>
2022-01-19 15:02:11 +0000

    pass to encrypt

--- application/controllers/gnu_encryptor.py
+++ application/controllers/gnu_encryptor.py
@@ -7,7 +7,7 @@ logger = wrap_logger(logging.getLogger(__name__))
 
 
 class GNUEncrypter:
-    def __init__(self, public_key, passphrase=None, always_trust=True, extra_args="--output /dev/null"):
+    def __init__(self, public_key, passphrase=None, always_trust=True):
         self.gpg = gnupg.GPG()
         self.gpg.import_keys(public_key.encode("utf-8"))
 
@@ -19,7 +19,7 @@ class GNUEncrypter:
         :param recipient: who is it for
         :return: string of encrypted data
         """
-        enc_data = self.gpg.encrypt(payload, recipient, always_trust=True)
+        enc_data = self.gpg.encrypt(payload, recipient, always_trust=True, extra_args="--output /dev/null")
         if not enc_data.ok:
             logger.error(
                 "Failed to encrypt with gpg", status=enc_data.status, error=enc_data.stderr, recipient=recipient
