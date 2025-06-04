98e70ceb4ae67f6bbe9d68b170f12712ac4bd823
Mark Price <mark.price@ons.gov.uk>
2025-06-04 14:59:16 +0100

    Lint

--- application/views/registry_instrument_view.py
+++ application/views/registry_instrument_view.py
@@ -61,15 +61,15 @@ def get_registry_instrument(exercise_id, form_type):
 
     # stubbing the above until I map the returned model to the json
     stubbed_registry_instrument = {
-            "exercise_id": exercise_id,
-            "instrument_id": "dfc3ddd7-3e79-4c6b-a8f8-1fa184cdd06b",
-            "survey_id": "0b1f8376-28e9-4884-bea5-acf9d709464e",
-            "classifier_type": "form_type",
-            "classifier_value": form_type,
-            "ci_version": 1,
-            "guid": "c046861a-0df7-443a-a963-d9aa3bddf328",
-            "published_at": "2025-12-31T00:00:00",
-        }
+        "exercise_id": exercise_id,
+        "instrument_id": "dfc3ddd7-3e79-4c6b-a8f8-1fa184cdd06b",
+        "survey_id": "0b1f8376-28e9-4884-bea5-acf9d709464e",
+        "classifier_type": "form_type",
+        "classifier_value": form_type,
+        "ci_version": 1,
+        "guid": "c046861a-0df7-443a-a963-d9aa3bddf328",
+        "published_at": "2025-12-31T00:00:00",
+    }
 
     if registry_instrument:
         return make_response(jsonify(stubbed_registry_instrument), 200)
