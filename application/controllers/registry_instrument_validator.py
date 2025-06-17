91d8d90023a841316f91ca50cc671ea4f07c3828
Mark Price <mark.price@ons.gov.uk>
2025-06-17 16:16:46 +0100

    Move payload validation

---
+++ application/controllers/registry_instrument_validator.py
@@ -0,0 +1,44 @@
+import datetime
+import re
+
+from application.controllers.helper import validate_uuid
+
+EXPECTED_KEYS = {
+    "survey_id",
+    "exercise_id",
+    "instrument_id",
+    "classifier_type",
+    "classifier_value",
+    "ci_version",
+    "guid",
+    "published_at",
+}
+
+
+def validate_registry_instrument_payload(payload, exercise_id):
+    payload_keys = set(payload.keys())
+    if payload_keys != EXPECTED_KEYS:
+        return False, f"Invalid payload keys. Expected: {EXPECTED_KEYS}"
+    if payload["exercise_id"] != exercise_id:
+        return False, "exercise_id in payload does not match path parameter"
+    if not validate_uuid(exercise_id):
+        return False, "Invalid exercise_id"
+    if not validate_uuid(payload["survey_id"]):
+        return False, "Invalid survey_id"
+    if not validate_uuid(payload["instrument_id"]):
+        return False, "Invalid instrument_id"
+    if payload["classifier_type"] not in ["form_type"]:
+        return False, "Invalid classifier type"
+    if not re.fullmatch(r"\d{4}", str(payload["classifier_value"])):
+        return False, "Invalid classifier value"
+    try:
+        int(payload["ci_version"])
+    except (ValueError, TypeError):
+        return False, "Invalid ci_version"
+    if not validate_uuid(payload["guid"]):
+        return False, "Invalid guid"
+    try:
+        datetime.datetime.fromisoformat(payload["published_at"])
+    except (ValueError, TypeError):
+        return False, "Invalid published_at"
+    return True, None
--- application/views/registry_instrument_view.py
+++ application/views/registry_instrument_view.py
@@ -1,6 +1,4 @@
-import datetime
 import logging
-import re
 from http import HTTPStatus
 
 import structlog
@@ -8,8 +6,10 @@ from flask import Blueprint, jsonify, make_response, request
 from werkzeug.exceptions import BadRequest
 
 from application.controllers.basic_auth import auth
-from application.controllers.helper import validate_uuid
 from application.controllers.registry_instrument import RegistryInstrument
+from application.controllers.registry_instrument_validator import (
+    validate_registry_instrument_payload,
+)
 
 log = structlog.wrap_logger(logging.getLogger(__name__))
 
@@ -57,7 +57,7 @@ def put_registry_instrument(exercise_id):
     We currently only support "classifier_type": "form_type" (e.g. where "classifier_value" is "0001").
 
     :param exercise_id: An exercise id (UUID)
-    :payload format example
+    :valid payload format example
     {
         "survey_id": "0b1f8376-28e9-4884-bea5-acf9d709464e"
         "exercise_id": "3ff59b73-7f15-406f-9e4d-7f00b41e85ce",
@@ -75,6 +75,9 @@ def put_registry_instrument(exercise_id):
 
     try:
         payload = request.get_json(force=True)
+        is_valid, error = validate_registry_instrument_payload(payload, exercise_id)
+        if not is_valid:
+            return make_response(error, HTTPStatus.BAD_REQUEST)
     except BadRequest:
         return make_response("Invalid JSON payload", HTTPStatus.BAD_REQUEST)
 
@@ -84,80 +87,6 @@ def put_registry_instrument(exercise_id):
     ci_version = None
     guid = None
     published_at = None
-
-    # -------- Validate the payload --------
-
-    # TODO: this will be all moved to an appropriate class once stable before PR is opened for review
-
-    expected_keys = {
-        "survey_id",
-        "exercise_id",
-        "instrument_id",
-        "classifier_type",
-        "classifier_value",
-        "ci_version",
-        "guid",
-        "published_at",
-    }
-
-    payload_keys = set(payload.keys())
-
-    if payload_keys != expected_keys:
-        return make_response(f"Invalid payload keys. Expected: {expected_keys}", HTTPStatus.BAD_REQUEST)
-
-    if "exercise_id" in payload:
-        if payload["exercise_id"] != exercise_id:
-            return make_response("exercise_id in payload does not match path parameter", HTTPStatus.BAD_REQUEST)
-        if not validate_uuid(exercise_id):
-            # TODO: check the exercise_id exists in the ras_ci.exercise table
-            #       once the constraint is in place we could just rely on SQLAlchemy exception handling
-            return make_response("Invalid exercise_id", HTTPStatus.BAD_REQUEST)
-
-    if "survey_id" in payload:
-        survey_id = payload["survey_id"]
-        if not validate_uuid(survey_id):
-            # TODO: check the survey_id exists in the ras_ci.survey table
-            #       once the constraint is in place we could just rely on SQLAlchemy exception handling
-            return make_response("Invalid survey_id", HTTPStatus.BAD_REQUEST)
-
-    if "instrument_id" in payload:
-        instrument_id = payload["instrument_id"]
-        if not validate_uuid(instrument_id):
-            # TODO: check the instrument_id exists in the ras_ci.instrument table
-            #       once the constraint is in place we could just rely on SQLAlchemy exception handling
-            return make_response("Invalid instrument_id", HTTPStatus.BAD_REQUEST)
-
-    if "classifier_type" in payload:
-        classifier_type = payload["classifier_type"]
-        # currently we only support the "form_type" classifier
-        if classifier_type not in ["form_type"]:
-            return make_response("Invalid classifier type", HTTPStatus.BAD_REQUEST)
-
-    if "classifier_value" in payload:
-        classifier_value = payload["classifier_value"]
-        if not re.fullmatch(r"\d{4}", str(classifier_value)):
-            return make_response("Invalid classifier value", HTTPStatus.BAD_REQUEST)
-
-    if "ci_version" in payload:
-        ci_version = payload["ci_version"]
-        try:
-            ci_version = int(ci_version)
-        except (ValueError, TypeError):
-            return make_response("Invalid ci_version", HTTPStatus.BAD_REQUEST)
-
-    if "guid" in payload:
-        guid = payload["guid"]
-        if not validate_uuid(guid):
-            return make_response("Invalid guid", HTTPStatus.BAD_REQUEST)
-
-    if "published_at" in payload:
-        published_at = payload["published_at"]
-        try:
-            datetime.datetime.fromisoformat(published_at)
-        except (ValueError, TypeError):
-            return make_response("Invalid published_at", HTTPStatus.BAD_REQUEST)
-
-    # -------- Completed validating the payload --------
 
     success, is_new = RegistryInstrument().save_registry_instrument_for_exercise_id_and_formtype(
         survey_id=survey_id,
